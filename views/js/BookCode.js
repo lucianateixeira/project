b1d7797a46212b8c7bf9cb084ecce6d172fa33ca
LUCIANA TEIXEIRA <48061912+lucianateixeira@users.noreply.github.com>
2019-12-08 10:42:01 +0000

    Update and rename PaddysCafeCode.js to BookCode.js

---
+++ views/js/BookCode.js
@@ -0,0 +1,29 @@
+function highlightAudiobook(idTable, bShowAudio) {
+    // if bShowAudio is true, then we're highlighting audiobook
+    var i = 0;
+    var oTable = document.getElementById(idTable);
+    var oTBODY = oTable.getElementsByTagName('tbody')[0];
+    var aTRs = oTBODY.getElementsByTagName('tr');
+    // walk through each of the table rows and see if it has a 
+    // "audiobook" attribute on it.
+    for (i = 0; i < aTRs.length; i++) {
+        if (aTRs[i].getAttribute('audiobook') == "true") {
+            if (bShowAudio) {
+                aTRs[i].style.backgroundColor = "lightGreen";
+            } else {
+                aTRs[i].style.backgroundColor = "";
+            };
+        };
+    };
+};
+// Utility function for getting the parent tag of a given tag
+// but only of a certain type (i.e. a TR, a TABLE, etc.)
+function getParentTag(oNode, sParentType) {
+    var oParent = oNode.parentNode;
+    while (oParent) {
+        if (oParent.nodeName == sParentType)
+            return oParent;
+        oParent = oParent.parentNode;
+    };
+    return oParent;
+};
